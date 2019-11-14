8f320807da8940051edd357f6f0555a81b5f4da5
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-11-14 15:55:14 +0100

    getPlayerName fail

--- tests/PlayerTest.php
+++ tests/PlayerTest.php
@@ -2,7 +2,18 @@
 
 use PHPUnit\Framework\TestCase;
 
-final class PlayerTest extends TestCase
+class PlayerTest extends TestCase
 {
-   
+    /** @test */
+    
+    public function shouldGetPlayerName()
+    {
+        $sut = new Player("Kalle");
+
+        $actual = $sut->getPlayerName();
+
+        $expected = "Kalle";
+
+        $this->assertEquals($actual, $expected);
+    }
 }
